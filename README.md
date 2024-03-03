cd4094dc719cbb5986a7ccb9d8417760b8fd5cbf
Vladimir V <vlad-free@mail.ru>
2024-03-03 16:26:13 +0300

    first setun1958asm.s

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Emulator-Setun-1958-Assembler
+
 Эмулятор троичной машины 'СЕТУНЬ' SETUN-1958 VM на языке Assembler
+
+A virtual machine of ternary computer Setun, also known as "Small Automatic Digital Machine" written on Assembler.
+
+
+## 1. История
+
+- Дата создания:            02.03.2024
+- Дата редактирования:      02.03.2024
+- Версия:                   0.01
+- Автор:                    Vladimir V.
+- E-mail:                   askfind@ya.ru
+
+
+## 2. Запуск эмулятора SETUN-1958
+
+### RARS -- RISC-V Assembler and Runtime Simulator
+
+RARS, the RISC-V Assembler, Simulator, and Runtime, will assemble and simulate
+the execution of RISC-V assembly language programs. Its primary goal is to be
+an effective development environment for people getting started with RISC-V.
+
+```shell
+~rars$ java -jar rars.jar ./app_trits/setun1958asm.s
+RARS 1.6  Copyright 2003-2019 Pete Sanderson and Kenneth Vollmar
+
+Ternary small digital computer 'Setun' on RISC-V
+
+Program terminated by calling exit
+```
+
+# Ссылки
+
+ 1. *Materials on ternary computer science* - <http://ternarycomp.cs.msu.su/>
+ 2. *Emulator of the first ternary Soviet "Setun"* - <http://trinary.su/projects/>
+ 3. *Unique Setun based on the ternary code* - <https://habr.com/ru/company/ua-hosting/blog/273929/>
+ 4. *Group users in social network VK* - <https://vk.com/setunsu/>
+ 5. *Group users in social network Telegram* - <https://t.me/setun_1958/>
+ 6. *RARS -- RISC-V Assembler and Runtime Simulator* - Documentation for supported
+
+  * [instructions](https://github.com/TheThirdOne/rars/wiki/Supported-Instructions),
+  * [system calls](https://github.com/TheThirdOne/rars/wiki/Environment-Calls),
+  * [assembler directives](https://github.com/TheThirdOne/rars/wiki/Assembler-Directives) and
+  *
+  * more can be found on the [wiki](https://github.com/TheThirdOne/rars/wiki).
+    Documentation included in the download can be accessed via the help menu.
+
+# Contributing
+
+Everybody is invited and welcome to contribute to Setun VM.
+
+Приглашаем всех желающих внести свой посильный вклад в эмулятор SETUN-1958.
---
+++ setun1958asm.s
@@ -0,0 +1,193 @@
+#
+# Filename: "setun1958asm.s"
+#
+# Project: Троичная МЦВМ "Сетунь" 1958 года на языке ассемблера RISC-V
+#
+# Create date: 01.03.2024
+# Edit date:   02.03.2023
+#
+# Version:     0.01
+#
+# Author:      Vladimir V.
+# E-mail:      askfind@ya.ru
+#
+# GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+#
+
+
+#
+#  Использование регистров RISC-V для регистров "Сетунь"
+#
+# Регистры s0-s11 (x8,x9,x18 - x27)
+#
+# K(1:9)     s0,s1   - код команды (адрес ячейки оперативной памяти)
+# F(1:5)     s2,s3   - индекс регистр
+# C(1:5)     s4,s5   - программный счетчик
+# W(1:1)     s6,s7   - знак троичного числа
+#
+# S(1:18)    s8,s9   - аккумулятор
+# R(1:18)    s10,s11 - регистр множителя
+#
+# ph1(1:1)   t0,t1   - 1 разряд переполнения
+# ph2(1:1)   t0,t1   - 1 разряд переполнения
+#
+# MB(1:4)    t0,t1   - троичное число зоны магнитного барабана
+#
+# Дополнительные троичные переменные
+#
+# MR(1:9)    t2,t3  - временный регистр для обмена с FRAM
+# BRPNT(1:5) t2,t3  - точка остановки по значению программного счетчика
+#
+# TMP(1:36)  ?      - временная переменная для троичного числа
+#
+
+#
+#  Троичные типы данных памяти "Сетунь-1958"
+#
+
+
+#
+#  Описание тритов в троичном числе в поле бит
+#
+#  W(1)	 	W, {t1,t0} = {t1:[b0],t0:[b0]}
+#  A(1...5) 	A, {t1,t0} = {t1:[b4...b0],t0:[b4...b0]}
+#  K(1...9)	K, {t1,t0} = {t1:[b8...b0],t0:[b8...b0]}
+#  S(1...18)	S, {t1,t0} = {t1:[b17...b0],t0:[b17...b0]}
+#
+#
+# SIZE_WORD_SHORT (9)		 /* короткое слово 9-трит  */
+# SIZE_WORD_LONG (18)		 /* длинное слово  18-трит */
+# SIZE_WORD_LONG_LONG (36) /* двойное длинное слово  36-трит */
+
+#
+# Описание ферритовой памяти FRAM
+#
+# NUMBER_ZONE_FRAM (3)	 /* количество зон ферритовой памяти */
+# SIZE_ZONE_TRIT_FRAM (54) /* количнество коротких 9-тритных слов в зоне */
+# SIZE_ALL_TRIT_FRAM (162) /* всего количество коротких 9-тритных слов */
+#
+# SIZE_GRFRAM (2)		   /* количнество груп (0...2) в FRAM */
+# SIZE_GR_TRIT_FRAM (81) /* количнество коротких 9-тритных слов в зоне */
+
+#
+# Адреса зон ферритовой памяти FRAM
+#
+# ZONE_M_FRAM_BEG (-120) ' ----0 '
+# ZONE_M_FRAM_END (-41)  ' -++++ '
+# ZONE_0_FRAM_BEG (-40)  ' 0---0 '
+# ZONE_0_FRAM_END (40)   ' 0++++ '
+# ZONE_P_FRAM_BEG (42)   ' +---0 '
+# ZONE_P_FRAM_END (121) ' +++++ '
+#
+#
+# Описание магнитного барабана DRUM
+#
+# SIZE_TRIT_DRUM (1944)	 /* количество хранения коротких слов из 9-тритов */
+# SIZE_ZONE_TRIT_DRUM (54) /* количество 9-тритных слов в зоне */
+# NUMBER_ZONE_DRUM (36)	 /* количество зон на магнитном барабане */
+# ZONE_DRUM_BEG (5)		 /* 01-- */
+# ZONE_DRUM_END (40)		 /* ++++ */
+#
+#
+# Типы данных для виртуальной троичной машины "Сетунь-1958"
+#
+# typedef uintptr_t addr;
+#
+# typedef struct trs
+# {
+#	uint8_t l;	 /* длина троичного числа в тритах */
+#	uint32_t t1; /* троичное число FALSE,TRUE */
+#	uint32_t t0; /* троичное число NIL */
+# } trs_t;
+
+# typedef struct long_trs
+# {
+#	uint8_t l;	 /* длина троичного числа в тритах			*/
+#	uint64_t t1; /* троичное число FALSE,TRUE */
+#	uint64_t t0; /* троичное число NIL */
+# } long_trs_t;
+#
+
+
+.globl  main
+
+# Сообщение о проекте
+.data
+project:
+.string "Ternary small digital computer 'Setun' on RISC-V\n"
+
+#
+# Macros
+#
+
+# Сброс машины
+.macro reset
+	mv s0, zero
+	mv s1, zero
+	mv s2, zero
+	mv s3, zero
+	mv s4, zero
+	mv s5, zero
+	mv s6, zero
+	mv s7, zero
+	mv s8, zero
+	mv s9, zero
+	mv s10, zero
+	mv s11, zero
+	mv t0, zero
+	mv t1, zero
+	mv t2, zero
+	mv t3, zero
+.end_macro
+
+#
+#   Операции с тритами
+#   trit = {-1,0,1}  - трит значение трита
+#
+
+# void and_t(int8_t *a, int8_t *b, int8_t *s);
+.macro and_t
+	nop
+.end_macro
+
+# void xor_t(int8_t *a, int8_t *b, int8_t *s);
+.macro xor_t
+	nop
+.end_macro
+
+# void or_t(int8_t *a, int8_t *b, int8_t *s);
+.macro or_t
+	nop
+.end_macro
+
+#  - [ ] исправить вывод в 9-ном виде.
+# void not_t(int8_t *a, int8_t *s);
+.macro not_t
+	nop
+.end_macro
+
+# void sum_t(int8_t *a, int8_t *b, int8_t *p0, int8_t *s, int8_t *p1);
+.macro sum_t
+	nop
+.end_macro
+
+
+#
+# Работа троичной МЦВМ 'Сетунь' 1958 года
+#
+.text
+main:
+        # Сообщение о проекте
+	li a7, 64
+	li a0, 1
+	la a1, project
+	li a2, 51
+	ecall
+	
+        # Сброс setun1958asm
+        reset
+
+success:
+	li a0, 42
+	li a7, 93
+	ecall
